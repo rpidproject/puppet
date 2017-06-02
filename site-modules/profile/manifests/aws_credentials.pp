5783c13b14b48af6fcffd9582379265bb92e8b14
John Arundel <john@bitfieldconsulting.com>
2017-06-02 17:10:51 +0100

    Add AWS creds

--- site-modules/profile/manifests/aws_credentials.pp
+++ site-modules/profile/manifests/aws_credentials.pp
@@ -2,12 +2,12 @@
 class profile::aws_credentials {
   if lookup('aws::test_mode') {
     notice('AWS orchestration is using the TEST credentials')
-    $aws_access_key = hiera('aws_test_access_key')
-    $aws_secret_key = hiera('aws_test_secret_key')
+    $aws_access_key = hiera('aws::test_access_key')
+    $aws_secret_key = hiera('aws::test_secret_key')
   } else {
     notice('AWS orchestration is using the LIVE credentials')
-    $aws_access_key = hiera('aws_access_key')
-    $aws_secret_key = hiera('aws_secret_key')
+    $aws_access_key = hiera('aws::access_key')
+    $aws_secret_key = hiera('aws::secret_key')
   }
 
   file { '/root/.aws':
