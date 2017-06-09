c068e1182734953ddacca8a127393c142158ec30
Bridget Almas <balmas@gmail.com>
2017-06-09 15:43:32 +0000

    starting to build the cordra docker image

---
+++ site-modules/profile/manifests/cordra/build.pp
@@ -0,0 +1,49 @@
+# Build the Handle Server Docker image
+class profile::cordra::build {
+  include 'Docker'
+
+  file { '/docker': 
+    ensure => directory,
+  }
+
+  file { '/docker/cordra':
+    ensure => directory,
+  }
+
+  file { '/docker/cordra/config/':
+    ensure => directory,
+  }
+
+  file { '/docker/cordra/Dockerfile':
+    source => 'puppet:///modules/profile/cordra/Dockerfile.cordra',
+    notify => Docker::Image['rpid-cordra'],
+  }
+
+  file { '/docker/cordra/config/config.json':
+    content => epp('profile/cordra/config.json.epp', {
+      'zookeeper_host' => hiera('cordra::zookeeper_host'),
+      'mongodb_host'   => hiera('cordra::mongodb_host'),
+    }),
+    notify => Docker::Image['rpid-cordra'],
+  }
+
+  file { '/docker/cordra/config/repoInit.json':
+    content => epp('profile/cordra/repoInit.json.epp', {
+      'admin_password' => hiera('cordra::admin_password'),
+      'handle_prefix'  => hiera('site::handle_prefix'),
+    }),
+    notify => Docker::Image['rpid-cordra'],
+  }
+
+  file { '/docker/cordra/config/web.xml':
+    content => epp('profile/cordra/web.xml.epp', {
+      'zookeeper_host' => hiera('cordra::zookeeper_host'),
+    }),
+    notify => Docker::Image['rpid-cordra'],
+  }
+
+  docker::image { 'rpid-cordra':
+    ensure      => latest,
+    docker_dir => '/docker/cordra',
+  }
+}
