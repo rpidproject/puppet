710be6165c0cf5775fe191a3879577cf7b324c8f
John Arundel <john@bitfieldconsulting.com>
2017-06-16 13:30:33 +0000

    Move Cordra config back into build phase

--- site-modules/cordra/manifests/build.pp
+++ site-modules/cordra/manifests/build.pp
@@ -3,12 +3,44 @@ class cordra::build {
   $docker_build_dir = lookup('docker_build_dir', String)
   $cordra_build_dir = "${docker_build_dir}/cordra"
 
-  file { $cordra_build_dir:
+  file { [
+    $cordra_build_dir,
+    "${cordra_build_dir}/config"
+  ]:
     ensure => directory,
   }
 
   file { "${cordra_build_dir}/Dockerfile":
     source => 'puppet:///modules/cordra/Dockerfile.cordra',
+    notify => Docker::Image['rpid-cordra'],
+  }
+
+  file { "${cordra_build_dir}/config/config.json":
+    content => epp('cordra/config.json.epp',
+      {
+        'zookeeper_host' => lookup('cordra::zookeeper_host', String),
+        'mongodb_host'   => lookup('cordra::mongodb_host', String),
+      }
+    ),
+    notify => Docker::Image['rpid-cordra'],
+  }
+
+  file { "${cordra_build_dir}/config/repoInit.json":
+    content => epp('cordra/repoInit.json.epp',
+      {
+        'admin_password' => lookup('cordra::admin_password', String),
+        'handle_prefix'  => lookup('site::handle_prefix', String),
+      }
+    ),
+    notify => Docker::Image['rpid-cordra'],
+  }
+
+  file { "${cordra_build_dir}/config/web.xml":
+    content => epp('cordra/web.xml.epp',
+      {
+        'zookeeper_host' => lookup('cordra::zookeeper_host', String),
+      }
+    ),
     notify => Docker::Image['rpid-cordra'],
   }
 
--- site-modules/cordra/manifests/server.pp
+++ site-modules/cordra/manifests/server.pp
@@ -3,10 +3,7 @@ class cordra::server {
   $docker_run_dir = lookup('docker_run_dir', String)
   $cordra_run_dir = "${docker_run_dir}/cordra"
 
-  file { [
-    $cordra_run_dir,
-    "${cordra_run_dir}/config"
-  ]:
+  file { $cordra_run_dir:
     ensure => directory,
   }
 
@@ -15,39 +12,9 @@ class cordra::server {
       {
         'zookeeper_host' => lookup('cordra::zookeeper_host', String),
         'mongodb_host'   => lookup('cordra::mongodb_host', String),
-        'config_dir'     => "${cordra_run_dir}/config",
         'cordra_tag'     => lookup('cordra::cordra_tag', String),
         'mongodb_tag'    => lookup('cordra::mongodb_tag', String),
         'zookeeper_tag'  => lookup('cordra::zookeeper_tag', String),
-      }
-    ),
-    notify  => Docker_compose["${cordra_run_dir}/docker-compose.yml"]
-  }
-
-  file { "${cordra_run_dir}/config/config.json":
-    content => epp('cordra/config.json.epp',
-      {
-        'zookeeper_host' => lookup('cordra::zookeeper_host', String),
-        'mongodb_host'   => lookup('cordra::mongodb_host', String),
-      }
-    ),
-    notify  => Docker_compose["${cordra_run_dir}/docker-compose.yml"]
-  }
-
-  file { "${cordra_run_dir}/config/repoInit.json":
-    content => epp('cordra/repoInit.json.epp',
-      {
-        'admin_password' => lookup('cordra::admin_password', String),
-        'handle_prefix'  => lookup('site::handle_prefix', String),
-      }
-    ),
-    notify  => Docker_compose["${cordra_run_dir}/docker-compose.yml"]
-  }
-
-  file { "${cordra_run_dir}/config/web.xml":
-    content => epp('cordra/web.xml.epp',
-      {
-        'zookeeper_host' => lookup('cordra::zookeeper_host', String),
       }
     ),
     notify  => Docker_compose["${cordra_run_dir}/docker-compose.yml"]
