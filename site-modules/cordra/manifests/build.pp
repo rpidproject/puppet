55a45847ba1c17a4f457e5ab4b86c401a32a8756
Bridget Almas <balmas@gmail.com>
2017-07-11 18:20:43 +0000

    run cordra setup if keys don't exist

--- site-modules/cordra/manifests/build.pp
+++ site-modules/cordra/manifests/build.pp
@@ -3,10 +3,7 @@ class cordra::build {
   $docker_build_dir = lookup('docker_build_dir', String)
   $cordra_build_dir = "${docker_build_dir}/cordra"
 
-  file { [
-    $cordra_build_dir,
-    "${cordra_build_dir}/config"
-  ]:
+  file { $cordra_build_dir:
     ensure => directory,
   }
 
