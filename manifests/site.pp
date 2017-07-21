cb4e4e72ad9b4d494a20a65b08b0017d0a6d9b27
John Arundel <john@bitfieldconsulting.com>
2017-07-21 16:04:04 +0000

    Move firewall setup to site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,1 +1,14 @@
+# Manage puppetlabs-firewall global setup
+resources { 'firewall':
+    purge => true,
+}
+
+Firewall {
+  before  => Class['profile::firewall_post'],
+  require => Class['profile::firewall_pre'],
+}
+
+class { ['profile::firewall_pre', 'profile::firewall_post']: }
+class { 'firewall': }
+
 include(lookup('classes', { 'merge' =>  'unique' }))
