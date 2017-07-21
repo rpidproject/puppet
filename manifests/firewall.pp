dd8b1bfffe0a519ba5ec1fb52ef8670e1840aec6
John Arundel <john@bitfieldconsulting.com>
2017-07-21 16:43:19 +0100

    Add firewall setup

---
+++ manifests/firewall.pp
@@ -0,0 +1,12 @@
+# Manage puppetlabs-firewall global setup
+resources { 'firewall':
+    purge => true,
+}
+
+Firewall {
+  before  => Class['profile::firewall_post'],
+  require => Class['profile::firewall_pre'],
+}
+
+class { ['profile::firewall_pre', 'profile::firewall_post']: }
+class { 'firewall': }
