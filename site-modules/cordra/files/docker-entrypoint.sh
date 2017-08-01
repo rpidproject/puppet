620e86ccbaabc1113e94d88f51b1ab06a5e76803
Bridget Almas <balmas@gmail.com>
2017-08-01 18:44:56 +0000

    trying to get the handle service and cordra playing nice

--- site-modules/cordra/files/docker-entrypoint.sh
+++ site-modules/cordra/files/docker-entrypoint.sh
@@ -1,8 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
 exec "$@"
-~                                                                               
-~                                                                               
-~                                                                               
-~               
--- site-modules/cordra/manifests/build.pp
+++ site-modules/cordra/manifests/build.pp
@@ -3,12 +3,32 @@ class cordra::build {
   $docker_build_dir = lookup('docker_build_dir', String)
   $docker_run_dir = lookup('docker_run_dir', String)
   $cordra_build_dir = "${docker_build_dir}/cordra"
-  $handle_admin_id = lookup('handle::config.server.admin_id',String)
-  $handle_admin_key = "${docker_run_dir}/handle/admpriv.bin" 
   $assigned_prefix = lookup('cordra::config.server.assigned_prefix',String)
+  $admin_idx = lookup('handle::config.server.admin_idx')
+  $site_handle = lookup('site::handle_prefix')
+  $handle_admin_id = "${$admin_idx}/${site_handle}"
+  $handle_run_dir = "${docker_run_dir}/handle"
+  $handle_admin_key = "${handle_run_dir}/handle/admpriv.bin" 
+  $cordra_user = 'cordra'
 
   file { $cordra_build_dir:
     ensure => directory,
+  }
+
+  file { "${cordra_build_dir}/handle":
+    ensure  => directory,
+    require => File[$cordra_build_dir],
+  }
+
+  file { "${cordra_build_dir}/handle/siteinfo.json":
+    ensure  => directory,
+    source  => "${handle_run_dir}/siteinfo.json",
+    require    => Docker::Run['rpid-handle'],
+  }
+
+  file { "${cordra_build_dir}/handle/admpriv.bin":
+    ensure  => directory,
+    source  => "${handle_run_dir}/admpriv.bin",
   }
 
   file { "${cordra_build_dir}/docker-entrypoint.sh":
@@ -17,14 +37,15 @@ class cordra::build {
     notify  => Docker::Image['rpid-cordra'],
   }
 
-  file { "${cordra_build_dir}/setup.cfg":
-    content => epp('cordra/setup.cfg.epp',
+  file { "${cordra_build_dir}/setup.cmd":
+    content => epp('cordra/setup.cmd.epp',
       {
         'assigned_prefix'  => $assigned_prefix,
         'handle_admin_id'  => $handle_admin_id,
-        'handle_admin_key' => $handle_admin_key,
+        'handle_admin_key' => '/cordra/handle/admpriv.bin',
       }
     ),
+    notify  => Docker::Image['rpid-cordra'],
   }
 
   file { "${cordra_build_dir}/Dockerfile":
@@ -33,6 +54,7 @@ class cordra::build {
         'docker_base'         => lookup('cordra::docker_base'),
         'ports'               => lookup('cordra::config.ports', Hash, 'hash'),
         'cordra_download_url' => lookup('cordra::download_url'),
+        'cordra_user'         => $cordra_user,
       }
     ),
     notify => Docker::Image['rpid-cordra'],
@@ -41,6 +63,5 @@ class cordra::build {
   docker::image { 'rpid-cordra':
     ensure     => latest,
     docker_dir => $cordra_build_dir,
-    require    => Docker::Image['rpid-handle'],
   }
 }
