734bfd4bfca9cdfbc67d48d9594af20571c73d99
Bridget Almas <balmas@gmail.com>
2017-08-03 13:50:45 -0400

    fix #23 again make rpid a node not a role

--- site-modules/role/manifests/rpid.pp
+++
@@ -1,9 +0,0 @@
-# Be the RPID node
-class role::rpid {
-  include profile::handle::build
-  include profile::handle::server
-  include profile::cordra::build
-  include profile::cordra::server
-  include profile::collections::build
-  include profile::collections::server
-}
