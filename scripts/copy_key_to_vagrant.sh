b0e11e6e7c8ec5a2a68dd8e911a42bb52ade1bc8
Bridget Almas <balmas@gmail.com>
2017-08-04 13:51:30 -0400

    removing site specific config

--- scripts/copy_key_to_vagrant.sh
+++ scripts/copy_key_to_vagrant.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gpg --export-secret-subkeys --export-options export-reset-subkey-passwd -a AD9793634FC0755D |vagrant ssh -c 'HOME=/root sudo gpg --import'
+gpg --export-secret-subkeys --export-options export-reset-subkey-passwd -a xxxxx |vagrant ssh -c 'HOME=/root sudo gpg --import'
